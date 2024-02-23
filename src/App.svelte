2925303a4d0e34bb743457f4f4e2b0de92589e13
Muhammad Hassan Jawwad <113373773+hassanjawwad12@users.noreply.github.com>
2024-02-23 16:53:11 +0500

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Feedbacklist from "./components/Feedbacklist.svelte";
+  import Stats from "./components/Stats.svelte";
+  import Form from "./components/Form.svelte";
+</script>
+
+<main class="container">
+  <Form />
+  <Stats/>
+  <Feedbacklist />
+</main>
